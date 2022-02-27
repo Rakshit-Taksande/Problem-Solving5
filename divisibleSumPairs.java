469790b502461ac6f420400da6c940ecf2c70eb2
Rakshit-Taksande <58409317+Rakshit-Taksande@users.noreply.github.com>
2022-02-28 00:17:21 +0530

    Add files via upload

---
+++ divisibleSumPairs.java
@@ -0,0 +1,91 @@
+/**
+Given an array of integers and a positive integer , determine the number of  pairs where  and  +  is divisible by .
+
+Example
+
+
+
+Three pairs meet the criteria:  and .
+
+Function Description
+
+Complete the divisibleSumPairs function in the editor below.
+
+divisibleSumPairs has the following parameter(s):
+
+int n: the length of array 
+int ar[n]: an array of integers
+int k: the integer divisor
+Returns
+- int: the number of pairs
+
+Input Format
+
+The first line contains  space-separated integers,  and .
+The second line contains  space-separated integers, each a value of .
+
+Constraints
+
+Sample Input
+
+STDIN           Function
+-----           --------
+6 3             n = 6, k = 3
+1 3 2 6 1 2     ar = [1, 3, 2, 6, 1, 2]
+Sample Output
+
+ 5
+ */
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        int count=0;
+       Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int a[] = new int[n];
+        for(int a_i=0; a_i < n; a_i++){
+            a[a_i] = in.nextInt();
+        }
+        
+        for(int i=0;i<(n-1);i++){
+            for(int j=0;j<n;j++){
+                if(i<j){
+                    if((a[i]+a[j])%k==0)
+                        count++;
+                }
+            }
+        }
+        System.out.println(count); 
+    }
+    }
+
+
+
+ /**
+Input (stdin):-
+
+6 3
+1 3 2 6 1 2
+
+Your Output (stdout):-
+5
+
+
+Expected Output
+5
+
+ */
